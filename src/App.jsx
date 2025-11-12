3253ba832a9507be01ec537726a9b02a733ed4df
Ritik-rao8 <ydv321@gmail.com>
2025-11-12 17:46:33 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import { BrowserRouter as Router,Routes,Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 import './App.css'
-import Navbar from './components/navbar'
-import Homepage from './pages/homepage'
 import Login from './pages/login'
 import Signup from './pages/signup'
 
-function AppContent() {
-  const location = useLocation();
-  const hideNavbarRoutes = ['/login', '/signup'];
-  const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
-
-  return (
-    <>
-      {shouldShowNavbar && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
-  );
-}
 
 function App() {
 
   return (
     <Router>
-      <AppContent />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
